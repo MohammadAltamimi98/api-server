8f914d237d937f6f280594a86c9c2b06f482806e
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-06-30 20:22:56 +0300

    again

---
+++ src/models/clothes.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const clothesSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  brand: { type: String }
+})
+
+
+const clothesModel = mongoose.model('clothesInst', clothesSchema);
+
+module.exports = clothesModel;
